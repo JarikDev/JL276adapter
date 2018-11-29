ab0ffbdfc8be12a46a3f74fffe9a4d9313758380
ivan4000 <sxln1@mail.ru>
2018-11-30 02:45:52 +0300

    JL276 adapter

---
+++ src/com/Main.java
@@ -0,0 +1,48 @@
+package com;
+
+import javafx.animation.TranslateTransition;
+
+public class Main {
+    public static void main(String[] args) {
+        CarWash carWash=new CarWash();
+        carWash.washCar(new Audi());
+        carWash.washCar(new TrackWrap(new MyTrack()));
+    }
+}
+class TrackWrap implements Car{
+    Track track;
+
+    public TrackWrap(Track track) {
+        this.track = track;
+    }
+
+    @Override
+    public void wash() {
+        track.clean();
+    }
+}
+
+interface Track{
+    void clean();
+}
+class MyTrack implements Track{
+    @Override
+    public void clean() {
+        System.out.println("Track is clean");
+    }
+}
+
+interface Car{
+    void wash();
+}
+class Audi implements Car{
+    @Override
+    public void wash() {
+        System.out.println("wash car");
+    }
+}
+class   CarWash{
+    public void washCar(Car car){
+        car.wash();
+    }
+}
